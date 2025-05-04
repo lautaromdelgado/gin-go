4f524b2ebadc5609999db262e89136709dc69aef
lautaromdelgado <lautaromdelgado@gmail.com>
2025-05-04 14:46:52 -0300

    servidor basico web de gin en Golang

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.New()
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "Hello, World!")
+	})
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"status":  "success",
+			"message": "pong",
+		})
+	})
+
+	r.Run(":8080")
+}
